b9b96c62fd32df745df9763c83afdd282a3129b5
brunobdantas <brunobdantas@gmail.com>
2026-09-06 19:54:20 -0300

    feat: connect bridge to dynamic identity CTY and history intelligence

--- src/native/mshv_bridge/DxwMshvBridge.h
+++ src/native/mshv_bridge/DxwMshvBridge.h
@@ -8,15 +8,27 @@
 #include <QVector>
 
 #include "../dxw/CandidateScorer.h"
+#include "../dxw/CtyResolver.h"
 #include "../dxw/Ft8SlotClock.h"
 #include "../dxw/QsoStateMachine.h"
+#include "DxwHistoryCache.h"
+#include "../../HvTxW/hvqthloc.h"
 
 namespace dxw {
 
 class DxwMshvBridge final : public QObject {
     Q_OBJECT
 public:
-    explicit DxwMshvBridge(QString myCall, QObject* parent = nullptr);
+    explicit DxwMshvBridge(QString myCall,
+                           QString myGrid,
+                           QString band,
+                           QString appPath,
+                           QObject* parent = nullptr);
+
+    QString stationCall() const { return myCall_; }
+    QString stationGrid() const { return myGrid_; }
+    QString currentBand() const { return band_; }
+    bool historyAvailable() const noexcept { return history_.available(); }
 
 signals:
     void selectDecode(QStringList decode);
@@ -27,6 +39,8 @@ signals:
 public slots:
     void onDecode(QStringList decode);
     void onQsoLogged(QStringList);
+    void setStationIdentity(QString call, QString grid);
+    void setBand(QString band);
     void setArmed(bool armed);
     void setStrategy(int strategy);
     void halt();
@@ -40,15 +54,21 @@ private:
     static ExchangeStage exchangeStage(const QString& token);
     static QString findCqCall(const QStringList& tokens);
 
-    Candidate candidateFrom(const QStringList& decode, const QString& call) const;
+    Candidate candidateFrom(const QStringList& decode, const QString& call);
     void processActions(const std::vector<Action>& actions);
     void emitState();
     bool runnerUp(const QString& excluding, Candidate& result) const;
 
     QString myCall_;
+    QString myGrid_;
+    QString band_;
+    QString appPath_;
     Strategy strategy_{Strategy::Both};
     QsoStateMachine sm_;
     CandidateScorer scorer_;
+    CtyResolver cty_;
+    DxwHistoryCache history_;
+    HvQthLoc qth_;
     QTimer clock_;
     QVector<Candidate> candidates_;
     QHash<QString, QStringList> rawByCall_;
