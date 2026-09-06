05f199601ff4943e75bba78dcb8b16e289ecfb2d
brunobdantas <brunobdantas@gmail.com>
2026-09-06 18:26:21 -0300

    v0.5: add native candidate scorer interface

---
+++ src/native/dxw/CandidateScorer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Domain.h"
+#include <vector>
+
+namespace dxw {
+
+struct ScoreWeights {
+    int newDxcc{4000};
+    int newBand{150};
+    int newMode{100};
+    int newSlot{80};
+    int lotw{25};
+    int eqsl{10};
+    int watchlist{500};
+    int unconfirmed{200};
+    int workedPenalty{-150};
+    int cooldownPenalty{-2500};
+    int snrPerDb{2};
+    int distancePer1000Km{2};
+};
+
+class CandidateScorer final {
+public:
+    explicit CandidateScorer(ScoreWeights weights = {});
+
+    [[nodiscard]] ScoreBreakdown score(const Candidate& candidate) const noexcept;
+    [[nodiscard]] std::vector<RankedCandidate> rank(const std::vector<Candidate>& candidates) const;
+
+private:
+    ScoreWeights weights_;
+};
+
+} // namespace dxw
